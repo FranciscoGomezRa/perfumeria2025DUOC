7a37f13eac33bc790ff1d5e3294241245b845e2d
FranciscoGomezRa <franc.gomezr@duocuc.cl>
2025-05-05 18:57:13 -0400

    Primer commit: proyecto Spring Boot

---
+++ src/main/java/com/gsf/PerfulandiaSpring/PerfulandiaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gsf.PerfulandiaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsf/PerfulandiaSpring/PerfulandiaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsf.PerfulandiaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
