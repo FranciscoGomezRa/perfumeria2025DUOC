de292397037e31bd578bc3005439986bd798a6da
FranciscoGomezRa <franc.gomezr@duocuc.cl>
2025-05-05 22:28:25 -0400

    Se agrega MicroServicio de Sucursal

---
+++ MSVC-SUCURSAL/src/main/java/com/gsf/MSVC_SUCURSAL/MsvcSucursalApplication.java
@@ -0,0 +1,13 @@
+package com.gsf.MSVC_SUCURSAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcSucursalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcSucursalApplication.class, args);
+	}
+
+}
---
+++ MSVC-SUCURSAL/src/test/java/com/gsf/MSVC_SUCURSAL/MsvcSucursalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsf.MSVC_SUCURSAL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcSucursalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
