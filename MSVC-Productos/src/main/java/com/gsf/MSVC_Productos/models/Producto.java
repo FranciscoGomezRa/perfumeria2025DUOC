3153d28051ae3ac2fc48b52fd8d82118085d8c72
FranciscoGomezRa <franc.gomezr@duocuc.cl>
2025-05-06 20:02:45 -0400

    Atributos base de MSVC Productos

---
+++ MSVC-Productos/src/main/java/com/gsf/MSVC_Productos/models/Producto.java
@@ -0,0 +1,33 @@
+package com.gsf.MSVC_Productos.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name="productos")
+@Getter @Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id_producto")
+    private Long idProducto;
+
+    @Column(name= "descripcionProducto", nullable = false)
+    @NotNull(message = "El campo Descripcion debe incluir informacion")
+    private String descripcionProducto;
+
+    @Column(name="nombreProducto",nullable = false)
+    @NotNull(message="El campo debe Incluir nombre Producto")
+    private String nombreProducto;
+
+    @Column(name="precioProducto")
+    @NotNull(message="El campo Precio no puede estar Vacio")
+    private Float precioProducto;
+
+
+}
