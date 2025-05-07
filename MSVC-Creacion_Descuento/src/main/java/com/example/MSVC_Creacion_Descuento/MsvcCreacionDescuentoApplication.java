038ad32bc861e44948aa2f8949d4f5fcf69bde65
FranciscoGomezRa <franc.gomezr@duocuc.cl>
2025-05-06 20:12:24 -0400

    Mover package MSVC-Creacion_Descuento desde TesteoDuoc a main

---
+++ MSVC-Creacion_Descuento/src/main/java/com/example/MSVC_Creacion_Descuento/MsvcCreacionDescuentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MSVC_Creacion_Descuento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCreacionDescuentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCreacionDescuentoApplication.class, args);
+	}
+
+}
---
+++ MSVC-Creacion_Descuento/src/test/java/com/example/MSVC_Creacion_Descuento/MsvcCreacionDescuentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSVC_Creacion_Descuento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCreacionDescuentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
