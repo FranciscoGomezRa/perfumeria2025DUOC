47c5f5a2f74f6102371f1076a3812314371245dc
FranciscoGomezRa <franc.gomezr@duocuc.cl>
2025-05-23 23:37:21 -0400

    test

---
+++ msvc_productos/src/main/java/com/gsf/msvc_productos/dtos/ProductoEliminadoDTO.java
@@ -0,0 +1,15 @@
+package com.gsf.msvc_productos.dtos;
+
+
+import lombok.*;
+
+@Getter@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class ProductoEliminadoDTO {
+
+    private String mensajeProductoEliminado;
+
+}
