1248e69ae1ec8ff5ed4b5fc210a74eb567814c6a
FranciscoGomezRa <franc.gomezr@duocuc.cl>
2025-05-24 23:41:39 -0400

    test

--- msvc.cliente/src/main/java/com/perfulandia/msvc/cliente/MsvcClienteAplication.java
+++ msvc.cliente/src/main/java/com/perfulandia/msvc/cliente/MsvcClienteAplication.java
@@ -2,13 +2,12 @@ package com.perfulandia.msvc.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class Application {
+public class MsvcClienteAplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(MsvcClienteAplication.class, args);
 	}
 
 }
