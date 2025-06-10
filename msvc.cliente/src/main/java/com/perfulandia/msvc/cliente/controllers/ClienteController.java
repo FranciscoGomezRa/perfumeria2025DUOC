a5a15273e0fb7e0ff3928f54e1a9bbda8ade67c6
Simon Villar <sim.villar@duocuc.cl>
2025-06-10 09:29:36 -0400

    Mod de POM cliente para usart swagger, (app properties y cliente controller, nuevo codigo)

--- msvc.cliente/src/main/java/com/perfulandia/msvc/cliente/controllers/ClienteController.java
+++ msvc.cliente/src/main/java/com/perfulandia/msvc/cliente/controllers/ClienteController.java
@@ -3,6 +3,12 @@ package com.perfulandia.msvc.cliente.controllers;
 
 import com.perfulandia.msvc.cliente.models.Cliente;
 import com.perfulandia.msvc.cliente.services.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,17 +20,32 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/clientes")
 @Validated
+@Tag(name = "Clientes", description = "Operaciones Crud Clientes")
 
 public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
     @GetMapping
+    @Operation(summary = "Obtiene todos los clientes", description = "Devuelve un list de Clientes en el Body")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Operacion exitosa")})
     public ResponseEntity<List<Cliente>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(this.clienteService.findAll());
     }
 
+
     @GetMapping("/{id}")
+    @Operation(summary = "Obtiene un cliente", description = "A traves del id suministrado devuelve el cliente con esa id")
+    @ApiResponses ( value = {
+            @ApiResponse(responseCode = "200", description = "operacion exitosa"),
+            @ApiResponse(responseCode = "404", description = "Medico no encontrado"),
+    })
+    @Parameters(value = {
+            @Parameter(name="id", description = "Este es el id unico del cliente", required = true)
+    })
+
+
     public ResponseEntity<Cliente>findById(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK).body(clienteService.findById(id));
     }
