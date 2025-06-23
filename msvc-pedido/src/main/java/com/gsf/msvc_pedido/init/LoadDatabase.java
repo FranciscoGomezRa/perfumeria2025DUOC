0c025043acae6a8a737b5ee8459e09cc6103df94
FranciscoGomezRa <franc.gomezr@duocuc.cl>
2025-06-22 23:51:38 -0400

    Cambio MSVC PEDIDO

--- msvc-pedido/src/main/java/com/gsf/msvc_pedido/init/LoadDatabase.java
+++ msvc-pedido/src/main/java/com/gsf/msvc_pedido/init/LoadDatabase.java
@@ -15,28 +15,9 @@ public class LoadDatabase implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    @Autowired
-    PedidoRepository pedidoRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        Faker faker = new Faker(Locale.of("es","CL"));
-
-        if(pedidoRepository.count() == 0){
-            for(int i=0;i<100;i++){
-                Producto producto = new Producto();
-
-                String precio = faker.commerce().price();
-
-                producto.setNombreProducto(faker.commerce().productName());
-                producto.setDescripcionProducto(faker.commerce().brand());
-                producto.setPrecioProducto(Float.parseFloat(precio.replace(",",".")));
-                producto = pedidoRepository.save(producto);
-                log.info("El producto creado es {}", producto);
-            }
-        }
 
     }
-
-
 }
